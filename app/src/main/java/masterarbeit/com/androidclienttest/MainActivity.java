c885954b656b114774bc3dba081421512972fc54
Jariel1 <albskate@arcor.de>
2015-10-14 08:14:55 +0200

    v2

--- app/src/main/java/masterarbeit/com/androidclienttest/MainActivity.java
+++ app/src/main/java/masterarbeit/com/androidclienttest/MainActivity.java
@@ -19,107 +19,64 @@ public class MainActivity extends AppCompatActivity {
     String datum = "2011-01-01";
     String uhrzeit = "13:37";
     String wartezeit = "12";
+    String uhrzeitAnfang = "11:00:00";
+    String uhrzeitEnde = "14:00:00";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Instantiate Progress Dialog object
         prgDialog = new ProgressDialog(this);
-        // Set Progress Dialog Text
-        prgDialog.setMessage("Please wait...");
-        // Set Cancelable as False
+        prgDialog.setMessage("Bitte warten...");
         prgDialog.setCancelable(false);
     }
 
     public void Update(View view) {
-
         RequestParams params = new RequestParams();
-        // Put Http parameter name with value of Name Edit View control
         params.put("datum", datum);
-        // Put Http parameter username with value of Email Edit View control
         params.put("uhrzeit", uhrzeit);
-        // Put Http parameter password with value of Password Edit View control
         params.put("wartezeit", wartezeit);
-        // Invoke RESTful Web Service with Http parameters
         update(params);
-
     }
 
 
     public void update(RequestParams params){
-        // Show Progress Dialog
         prgDialog.show();
-        // Make RESTful webservice call using AsyncHttpClient object
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://192.168.2.111:8080/useraccount/register/doregister", params, new AsyncHttpResponseHandler() {
-            // When the response returned by REST has Http response code '200'
+        client.get("http://192.168.2.111:8080/wartezeiten/update/doupdate", params, new AsyncHttpResponseHandler() {
+
             @Override
             public void onSuccess(String response) {
-                // Hide Progress Dialog
                 prgDialog.hide();
                 try {
-                    // JSON Object
                     JSONObject obj = new JSONObject(response);
-                    // When the JSON response has status boolean value assigned with true
-                    if (obj.getBoolean("status")) {
-                        // Display successfully registered message using Toast
-                        Toast.makeText(getApplicationContext(), "You are successfully registered!", Toast.LENGTH_LONG).show();
-                    }
-                    // Else display error message
-                    else {
-                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
-                    }
+                    Toast.makeText(getApplicationContext(), obj.getString("antwort"), Toast.LENGTH_LONG).show();
                 } catch (JSONException e) {
-                    // TODO Auto-generated catch block
-                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
-
                 }
             }
 
-            // When the response returned by REST has Http response code other than '200'
             @Override
             public void onFailure(int statusCode, Throwable error,
                                   String content) {
-                // Hide Progress Dialog
                 prgDialog.hide();
-                // When Http response code is '404'
-                if (statusCode == 404) {
-                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code is '500'
-                else if (statusCode == 500) {
-                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code other than 404, 500
-                else {
-                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
-                }
+                Toast.makeText(getApplicationContext(), "Unexpected Error occcured!", Toast.LENGTH_LONG).show();
             }
         });
     }
 
     public void Select(View view) {
-
         RequestParams params = new RequestParams();
-        // Put Http parameter name with value of Name Edit View control
         params.put("datum", datum);
-        // Put Http parameter username with value of Email Edit View control
         select(params);
-
-
     }
 
     public void select(RequestParams params){
-        // Show Progress Dialog
         prgDialog.show();
-
-        // Make RESTful webservice call using AsyncHttpClient object
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://192.168.2.111:8080/useraccount/login/dologin", params, new AsyncHttpResponseHandler() {
-            // When the response returned by REST has Http response code '200'
+        client.get("http://192.168.2.111:8080/wartezeiten/select/doselect", params, new AsyncHttpResponseHandler() {
+
             @Override
             public void onSuccess(String response) {
                 // Hide Progress Dialog
@@ -134,27 +91,90 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-            // When the response returned by REST has Http response code other than '200'
             @Override
             public void onFailure(int statusCode, Throwable error,
                                   String content) {
-                // Hide Progress Dialog
+
                 prgDialog.hide();
-                // When Http response code is '404'
-                if (statusCode == 404) {
-                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code is '500'
-                else if (statusCode == 500) {
-                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code other than 404, 500
-                else {
-                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
-                }
+                Toast.makeText(getApplicationContext(), "Unexpected Error occcured!", Toast.LENGTH_LONG).show();
             }
         });
     }
+
+
+    public void Vorschlag(View view) {
+        RequestParams params = new RequestParams();
+        params.put("datum", datum);
+        params.put("uhrzeitAnfang", uhrzeitAnfang);
+        params.put("uhrzeitEnde", uhrzeitEnde);
+        vorschlag(params);
+    }
+
+    public void vorschlag(RequestParams params){
+        prgDialog.show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://192.168.2.111:8080/wartezeiten/vorschlag/dovorschlag", params, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(String response) {
+                prgDialog.hide();
+
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), obj.getString("uhrzeit") + " " + obj.getString("wartezeit"), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                prgDialog.hide();
+                Toast.makeText(getApplicationContext(), "Unexpected Error occcured!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    public void VorschlagAnnahme(View view) {
+        RequestParams params = new RequestParams();
+        params.put("datum", datum);
+        params.put("uhrzeitAnfang", uhrzeitAnfang);
+        params.put("uhrzeitEnde", uhrzeitEnde);
+        vorschlagAnnahme(params);
+    }
+
+    public void vorschlagAnnahme(RequestParams params){
+        prgDialog.show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://192.168.2.111:8080/wartezeiten/vorschlagannahme/dovorschlagannahme", params, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(String response) {
+                prgDialog.hide();
+
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), obj.getString("antwort"), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                prgDialog.hide();
+                Toast.makeText(getApplicationContext(), "Unexpected Error occcured!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+
+
 
 
 
